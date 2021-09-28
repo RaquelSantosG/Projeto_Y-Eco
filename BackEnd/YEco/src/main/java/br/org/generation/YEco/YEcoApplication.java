545bfd425be13edbfec5e0ae51790421f5437f78
raquelnew <raquelnew3@gmail.com>
2021-09-28 17:51:05 -0300

    Projeto criado

---
+++ BackEnd/YEco/src/main/java/br/org/generation/YEco/YEcoApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.YEco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YEcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YEcoApplication.class, args);
+	}
+
+}
---
+++ BackEnd/YEco/src/test/java/br/org/generation/YEco/YEcoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.YEco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YEcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
