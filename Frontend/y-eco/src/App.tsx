2b48cea4298d9cb8e3cd433f83f9a82b3eb9a673
Julio Balbino <julio4110@hotmail.com>
2021-11-03 01:48:17 -0300

    Adicionando navbar

--- Frontend/y-eco/src/App.tsx
+++ Frontend/y-eco/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/statics/navbar/Navbar';
+
+
+
+
 
 function App() {
   return (
     <>
-    
+      <Navbar />
     </>
   );
 }
--- Frontend/y-eco/src/components/statics/navbar/Navbar.tsx
+++ Frontend/y-eco/src/components/statics/navbar/Navbar.tsx
@@ -1,1 +1,124 @@
 import './Navbar.css';
+import React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import { AppBar, Grid, Toolbar, Typography } from '@material-ui/core';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { Box } from '@material-ui/system';
+
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
+function Navbar() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent='center' alignItems="center" >
+                <Grid alignItems="center" item xs={12}>
+                    <Box className='logo'>
+                        <Box display='flex' alignItems="center" justifyContent="center">
+                            <Typography variant="h2">Y-Eco</Typography>
+                        </Box>
+                        <Box display='flex' alignItems="center" justifyContent="center">
+                            <Typography variant="h5">Produtos sustentáveis, ecológicos e biodegradáveis</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+
+            <AppBar position="static">
+                <Toolbar className='menu' variant="dense" >
+                    <Box display='flex' justifyContent='center' className='boxToolbar'>
+                        <Box mx={5} className='cursor'>
+                            <HomeRoundedIcon className='icon' />
+                            <Typography variant="h6" color="inherit">
+                                home
+                            </Typography>
+                        </Box>
+
+                        <Box mx={5} className='cursor'>
+                            <ShoppingBagIcon className='icon' />
+                            <Typography variant="h6" color="inherit">
+                                produtos
+                            </Typography>
+                        </Box>
+
+                        <Box mx={5} className='cursor'>
+                            <DashboardIcon className='icon' />
+                            <Typography variant="h6" color="inherit">
+                                categoria
+                            </Typography>
+                        </Box>
+
+                        <Box mx={5} className='cursor'>
+                            <FavoriteIcon className='icon' />
+                            <Typography variant="h6" color="inherit">
+                                doações
+                            </Typography>
+                        </Box>
+                    </Box>
+                    <Box display='flex' justifyContent='end' width='auto' className='boxToolbar'>
+                        <Search>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+
+                        <Box display='flex' justifySelf='flex-end' alignItems="center">
+                            <Typography variant="h6" color="inherit">
+                                logout
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
