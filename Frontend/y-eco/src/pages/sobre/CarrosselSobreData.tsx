14b5cb44b7935d75ff927d83ef7c9e48d6552535
Maysa <maysa.binho.perola@hotmail.com>
2021-11-15 11:07:21 -0300

    Melhorias e criação Carrossel

---
+++ Frontend/y-eco/src/pages/sobre/CarrosselSobreData.tsx
@@ -0,0 +1,9 @@
+import Img1 from './asset/imagensCarrossel/maysa.png';
+import Img2 from './asset/imagensCarrossel/bruno.png';
+import Img3 from './asset/imagensCarrossel/julio.png';
+import Img4 from './asset/imagensCarrossel/raquel.png';
+import Img5 from './asset/imagensCarrossel/marcio.png';
+import { useState } from 'react';
+
+
+export const images = [{ img: Img1} ,{ img: Img2}, {img: Img3}, { img: Img4}, { img: Img5}];
--- Frontend/y-eco/src/pages/sobre/Sobre.tsx
+++ Frontend/y-eco/src/pages/sobre/Sobre.tsx
@@ -1,221 +1,78 @@
 import React from 'react';
-import { ThemeProvider} from '@mui/system';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import {Button, Box, Paper} from '@material-ui/core';
-import { Grid, Typography } from '@material-ui/core'
+import {  Box  } from '@material-ui/core';
 import './Sobre.css';
-import { GitHub, WhatsApp } from '@material-ui/icons';
+import { GitHub } from '@material-ui/icons';
+import { Grid, Typography, Paper, Container } from '@mui/material';
+import CarrosselSobre from './CarrosselSobre';
 
-function Sobre(){
-    return(
-        <div>
+
+function Sobre() {
+    return (
+        <>
             <Grid container  >
+                <Grid item xs={6}>
 
-                <Grid item direction='column'>
                     <Box>
-                        <Box display="flex" justifyContent="center">
-                            <Box>
-                                <Box marginTop='50px' display="flex" justifyContent="center" >
-                                    <Typography variant="h4" color="#39796b">UM POUCO SOBRE NÓS</Typography>
-                                </Box>
+                        <Box marginTop='50px' display="flex" justifyContent="center" >
+                            <img src="https://cdn.discordapp.com/attachments/882496817550483510/908341953488359505/40313.jpg" alt="" style={{ width: "90%", borderRadius: '6%' }} />
+                        </Box>
+                        <Box display="flex" marginTop='50px' justifyContent="center" >
+                            <Typography variant="h4" color="#39796b">UM POUCO SOBRE NÓS</Typography>
+                        </Box>
+                        <Box p={5}>
 
-                                <Box p={5}>
-                                    <Typography variant="body1" color="#39796b">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptate enim labore porro obcaecati necessitatibus a, reprehenderit saepe optio ducimus perferendis eos odio vero. Dolores eos sint saepe quaerat quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, nostrum quasi doloremque corrupti aliquid doloribus mollitia delectus debitis autem enim eos odio quidem suscipit nam expedita quo consequatur quam perferendis!</Typography>
-                                </Box>
-                            </Box>
+                            <Typography variant="body1" color="#39796b">Aqui você encontra produtos ecológicos, reutilizáveis para o seu dia a dia ser mais sustentável. Pequenas atitudes que geram grandes mudanças! Fortalecer o consumo consciente e a mudança de hábito para preservar nosso planeta.</Typography>
+                        </Box>
+                    </Box>
+
+                </Grid>
+                <Grid item xs={6} >
+                    <Box>
+                        <Box marginTop='50px' display="flex" justifyContent="center" >
+                            <img src="https://cdn.discordapp.com/attachments/882496817550483510/908341950225203210/17877.jpg" alt="" style={{ width: "90%", borderRadius: '6%' }} />
+                        </Box>
+                        <Box display="flex" marginTop='50px' justifyContent="center" > <Typography variant="h4" color="#39796b">MISSÃO E VALORES!</Typography>
+                        </Box>
+
+                        <Box p={5}>
+                            <Typography variant="body1" color="#39796b">Missão:
+
+                                Fornecer dicas e práticas sustentáveis que gerem impactos positivos para o meio ambiente e para sociedade. Engajar cada vez mais pessoas a substituírem materiais de uso único por reutilizáveis.Tornar nossas ações diárias mais respeitosas com o meio ambiente. Proteger nossas futuras gerações da quantidade de resíduos que estão sendo descartados na natureza de forma desenfreada. </Typography>
+
+                            <Typography variant="body1" color="#39796b">Valores:
+
+                                - Amor à vida
+
+                                - Respeito incondicional à natureza
+
+                                - Equidade
+
+                                - Empatia
+
+                                - Ética e transparência</Typography>
+
+                        </Box>
+                    </Box>
 
 
-                        </Box>
 
+                </Grid>
+                <Grid container justifyContent="center" marginBottom='10px' className='teste'>
+                    <Box display="flex" justifyContent="center" > <Typography variant="h4" color='White'>DESENVOLVEDORES</Typography>
+                    </Box>
+                </Grid>
+                <Grid container >
+            <Container>
+                <Box justifyContent="center" pt={6} pb={10}  display="flex"  >
+                    <CarrosselSobre/>
+                </Box>
+            </Container>
+            </Grid>
+             
+            </Grid>
+        </>
 
-                    </Box>
-                </Grid>
-                <Paper sx={{ p:1, margin: 'auto', maxWidth:1500, flexGrow: 10 }}>
-                <Grid container direction="row" justifyContent="space-around" alignItems="stretch">
-                <Grid item xs={2}>
-                     <Box display="flex" justifyContent="center">
-                                    <Typography variant="h4" color="initial">MAYSA</Typography>
-                                </Box>
-                    <Box display="flex" justifyContent="center" mt={2} >
-                        <img src="https://avatars.githubusercontent.com/u/89356698?v=4" alt="" style={{ width: "50%", borderRadius: '50%'}} />
-                    </Box>
-                    <Box p={2}>
-                                    <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptate enim labore porro obcaecati necessitatibus a, reprehenderit saepe optio ducimus perferendis eos odio vero. !</Typography>
-                                </Box>
-                                <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#39796b", height: "120px", borderRadius:'20px' }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Contate - me </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/mays2pearl" target="_blank">
-                                <GitHub style={{ fontSize: 50, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/maysa-andrade-maysa-andrade/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ height: "60px" }}>
-                        <Box paddingTop={1}>
-                            
-                        </Box>
-                        <Box>
-                            
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-                </Grid>
-                <Grid item xs={2}>
-                     <Box display="flex" justifyContent="center">
-                                    <Typography variant="h4" color="initial">RAQUEL</Typography>
-                                </Box>
-                    <Box display="flex" justifyContent="center" mt={2} >
-                        <img src="https://avatars.githubusercontent.com/u/72945788?v=4" alt="" style={{ width: "50%", borderRadius: '50%' }} />
-                    </Box>
-                    <Box p={2}>
-                                    <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptate enim labore porro obcaecati necessitatibus a, reprehenderit saepe optio ducimus perferendis eos odio vero. </Typography>
-                                </Box>
-                                <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#39796b", height: "120px", borderRadius:'20px' }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Contate - me </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/RaquelSantosG" target="_blank">
-                                <GitHub style={{ fontSize: 50, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/raquel-santos-gomes-b21318187/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ height: "60px" }}>
-                        <Box paddingTop={1}>
-                            
-                        </Box>
-                        <Box>
-                            
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-                </Grid>
-                <Grid item xs={2}>
-                     <Box display="flex" justifyContent="center">
-                                    <Typography variant="h4" color="initial">BRUNO</Typography>
-                                </Box>
-                    <Box display="flex" justifyContent="center" mt={2} >
-                        <img src="https://avatars.githubusercontent.com/u/88937060?v=4" alt="" style={{ width: "50%", borderRadius: '50%'}} />
-                    </Box>
-                    <Box p={2}>
-                                    <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptate enim labore porro obcaecati necessitatibus a, reprehenderit saepe optio ducimus perferendis eos odio vero. </Typography>
-                                </Box>
-                                <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#39796b", height: "120px", borderRadius:'20px' }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Contate - me </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/Bmuraro" target="_blank">
-                                <GitHub style={{ fontSize: 50, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/brunomuraros" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ height: "60px" }}>
-                        <Box paddingTop={1}>
-                            
-                        </Box>
-                        
-                    </Box>
-                </Grid>
-            </Grid>
-                </Grid>
-                <Grid item xs={2}>
-                     <Box display="flex" justifyContent="center">
-                                    <Typography variant="h4" color="initial">JÚLIO</Typography>
-                                </Box>
-                    <Box display="flex" justifyContent="center" mt={2} >
-                        <img src="https://avatars.githubusercontent.com/u/57552535?v=4" alt="" style={{ width: "50%", borderRadius: '50%'}} />
-                    </Box>
-                    <Box p={2}>
-                                    <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptate enim labore porro obcaecati necessitatibus a, reprehenderit saepe optio ducimus perferendis eos odio vero. !</Typography>
-                                </Box>
-                                <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#39796b", height: "120px", borderRadius:'20px' }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Contate - me </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/juliobalbino" target="_blank">
-                                <GitHub style={{ fontSize: 50, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/juliobalbino/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ height: "60px" }}>
-                        <Box paddingTop={1}>
-                            
-                        </Box>
-                        
-                    </Box>
-                </Grid>
-            </Grid>
-                </Grid>
-                <Grid item xs={2}>
-                     <Box display="flex" justifyContent="center">
-                                    <Typography variant="h4" color="initial">MÁRCIO</Typography>
-                                </Box>
-                    <Box display="flex" justifyContent="center" mt={2} >
-                        <img src="https://avatars.githubusercontent.com/u/88987545?v=4" alt="" style={{ width: "50%", borderRadius: '50%' }} />
-                    </Box>
-                    <Box p={2}>
-                                    <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptate enim labore porro obcaecati necessitatibus a, reprehenderit saepe optio ducimus perferendis eos odio vero.</Typography>
-                                </Box>
-                                <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#39796b", height: "120px", borderRadius:'20px'}}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Contate - me </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center" >
-                            <a href="https://github.com/marciors92" target="_blank">
-                                <GitHub style={{ fontSize: 50, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/marciors92/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ height: "60px" }}>
-                        <Box paddingTop={1}>
-                            
-                        </Box>
-                        <Box>
-                            
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-                </Grid>
-                
-              
-                </Grid>
-            </Paper>
-            </Grid>
-        </div>
     )
 }
 
